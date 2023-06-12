9896e949dc6267c69160c5c8f16ee4e601a3c12f
Shubham <85667728+shubhamprabhu10@users.noreply.github.com>
2023-06-12 18:56:22 +0530

    Update and rename Frogs and Jumps, 26th March to 26th March, Frogs and Jumps.py

--- 26th March, Frogs and Jumps.py
+++ 26th March, Frogs and Jumps.py
@@ -1,5 +1,3 @@
-#User function Template for python3
-
 class Solution:
     def unvisitedLeaves(self, N, leaves, frogs):
         # code here
@@ -19,20 +17,3 @@ class Solution:
             if(y==0):
                 c=c+1
         return c
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-if __name__ == '__main__':
-    
-    T = int(input())
-    while T > 0: 
-        N, leaves = [int(i) for i in input().split()]
-        frogs = [int(i) for i in input().split()]
-        ob = Solution()
-        print(ob.unvisitedLeaves(N, leaves, frogs))
-        
-        T -= 1
-# } Driver Code Ends
