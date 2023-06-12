79d4f2eb469bc7cc2eaf49f5f8f2d6a4272c2376
Shubham <85667728+shubhamprabhu10@users.noreply.github.com>
2023-06-12 18:58:40 +0530

    Update and rename Minimum BST Sum Subtree, 18th April.py to 18th April, Minimum BST Sum Subtree.py

--- 18th April, Minimum BST Sum Subtree.py
+++ 18th April, Minimum BST Sum Subtree.py
@@ -1,25 +1,11 @@
-#User function Template for python3
-
-'''
-class Node:
-    def __init__(self,val):
-        self.data=val
-        self.left=None
-        self.right=None
-'''
 
 class Solution:
-    def check(self,node,mini,maxi):
-        
+    def check(self,node,mini,maxi):       
         if node is None:
             return True
- 
-    # False if this node violates min/max constraint
+        
         if node.data < mini or node.data > maxi:
            return False
- 
-    # Otherwise check the subtrees recursively
-    # tightening the min or max constraint
         return (self.check(node.left, mini, node.data - 1) and
             self.check(node.right, node.data+1, maxi))
      
@@ -33,16 +19,13 @@ class Solution:
         
         if left+right+root.data==target and self.check(root,mini,maxi)==True :
             if self.s  > l+r+1:
-                self.s=l+1+r
-                
-            
-         
-        return (left+right+root.data, l+r+1 )   
+                self.s=l+1+r                                     
+        return (left+right+root.data, l+r+1 )
+    
     def minSubtreeSumBST(self, target, root):
         
         self.s=10000   
-        ans,s=self.solve(root,0,target,0)
-        
+        ans,s=self.solve(root,0,target,0)        
         if self.s==10000:
             return -1
         return self.s
