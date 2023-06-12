bad97a92d42e6b5af1cd37ec6c8e91c1650742d8
Shubham <85667728+shubhamprabhu10@users.noreply.github.com>
2023-06-12 18:59:37 +0530

    Update and rename Last modified ball, 9th January. to 9th January, Last modified ball.py

--- 9th January, Last modified ball.py
+++ 9th January, Last modified ball.py
@@ -1,9 +1,6 @@
 class Solution():
-    def solve(self, N, A):
-        #your code here
+    def solve(self, N, A):        
         while N>0:
             if (A[N-1]<=8):
                 return N
-            N=N-1
-                
-  
+            N=N-1                
